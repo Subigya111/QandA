3e30294ab32fdaa21e98fe330baf079c0c16c11f
subigya <subigyapok08@gmail.com>
2026-06-21 21:21:40 +0545

    first commit

---
+++ app/Models/Question.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Question extends Model
+{
+    //
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+    public function answers():HasMany{
+        return $this->hasMany(Answer::class);
+    }
+}
