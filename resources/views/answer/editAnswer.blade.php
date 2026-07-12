8ec4ea9f60165b11d20babe74d749a7cba1b6700
subigya <subigyapok08@gmail.com>
2026-07-13 02:51:24 +0545

    fix:resolve font-style for navbar in answer page

--- resources/views/answer/editAnswer.blade.php
+++ resources/views/answer/editAnswer.blade.php
@@ -1,5 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <title>Edit Answer</title>
+@include('layouts.navbar');
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
     body{font-family:'Poppins',sans-serif;}
